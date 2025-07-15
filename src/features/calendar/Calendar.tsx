52860dfd11377f901c693dab5d28827f2ab2c9c4
Bianchi <9850842+athomasbianchi@users.noreply.github.com>
2025-07-14 22:41:49 -0400

    big calendar and some vacation and holidays

--- src/features/calendar/Calendar.tsx
+++ src/features/calendar/Calendar.tsx
@@ -5,18 +5,42 @@ dayjs.extend(dayOfYear)
 
 // import { useState } from "react"
 
-const today = dayjs()
-const year = Array.from({length: 365}, (_,i) => i + 1).map(x => dayjs().dayOfYear(x))
+const holidays = {
+  '01-01': true,
+  '07-04': true,
+}
+
+const year = Array.from({ length: 365 }, (_, i) => i + 1).map(x => dayjs().dayOfYear(x))
 
 export const Calendar = (): JSX.Element => {
-  console.log(today)
-  console.log(year)
 
   return (
-    <div>
-      <div>{today.format('MMM')}</div>
-      <div>{today.format('DD')}</div>
-      <div>{today.format('YYYY')}</div>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "14% 14% 14% 14% 14% 14% 14%",
+        width: '100%'
+      }}
+    >
+      {year.map(date => {
+        const day = date.day()
+        const month = date.month()
+        const holiday: boolean = holidays[date.format('MM-DD')]
+        console.log(day)
+        return (
+          <div
+            style={{
+              gridColumn: day+1,
+              backgroundColor: month % 2 ? '#d3d3d3' : 'white',
+              color: day === 0 || day === 6 ? "red" : '',
+              fontWeight: holiday ? 'bolder' : 'initial',
+              border: "black 1px solid",
+              height: '100%'
+            }}
+          >
+            {date.format("D")}
+          </div>)
+      })}
     </div>
   )
 
